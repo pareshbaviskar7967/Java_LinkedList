6279924afcd5afac1a61a92fe83d39389af45c14
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-09 00:13:15 +0530

    Solution for UC-7

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.Scanner;
+
 public class LinkedList {
 	Node head;
 
@@ -89,6 +91,25 @@ public class LinkedList {
 		last.next = temp.next;
 	}
 
+	public void searchNode(int data) {
+		Node n = head;
+		int count = 1;
+		int flag = 0;
+		while (n != null) {
+			if (n.data == data) {
+				flag = 1;
+				break;
+			}
+			count = count + 1;
+			n = n.next;
+		}
+
+		if (flag == 1)
+			System.out.println("Node with value " + data + " is found at position " + count);
+		else
+			System.out.println("Element is not present in the list");
+	}
+
 	void printList() {
 		Node currNode = head;
 		System.out.println("LinkedList: ");
@@ -100,6 +121,7 @@ public class LinkedList {
 	}
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		LinkedList object = new LinkedList();
 		object.append(56);
 		object.append(70);
@@ -115,5 +137,8 @@ public class LinkedList {
 		object.deleteLast();
 		System.out.println("\nAfter deleting the last element,");
 		object.printList();
+		System.out.println("\nEnter the element to search: ");
+		int node = scanner.nextInt();
+		object.searchNode(node);
 	}
 }
